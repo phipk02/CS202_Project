8f206d217276fd42a1285ed3d59a8f16f3f6894a
Phi Lê <phipk02@gmail.com>
2021-12-08 16:11:07 +0700

    add class involve in State, del/rename some files

---
+++ CS202_Project/include/StateIdentifiers.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef STATEIDENTIFIERS_H
+#define STATEIDENTIFIERS_H
+
+namespace States
+{
+	enum ID
+	{
+		None,
+		Menu, 		// done
+		Game, 		// done but level 1, level 2, level 3
+        Load, 		// dont but load function
+		Loading, 	// done
+		Pause, 		// done
+        Save, 		// dont but save function
+        End,  		//
+		Settings,	// 
+	};
+}
+
+#endif
